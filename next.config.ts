a3702fdbf6edaab79ec295c63e07e0cccf2c3cf3
saldanaj97 <saldanaj97@gmail.com>
2025-11-12 14:21:42 -0600

    refactor: remove unused webpack configuration for postgres

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['postgres'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('postgres');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
